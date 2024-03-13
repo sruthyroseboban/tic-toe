deb33bc9b6b2a298d3ba44dba96290acfed1f736
SRB <98799040+sruthyroseboban@users.noreply.github.com>
2024-03-13 14:33:53 +0400

    style changed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Board from './Components/board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+     <div className="App">
+      {/* <header className="App-header">
+      
+      </header>  */}
       <Board></Board>
-      </header>
     </div>
+    </>
   );
 }
 
